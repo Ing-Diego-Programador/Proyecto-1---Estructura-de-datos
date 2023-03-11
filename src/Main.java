36a0b25815d4ed5d4af8ac945c03637da31c3f0f
Diego Vásquez <vas211628@uvg.edu.gt>
2023-03-10 20:32:07 -0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Vector;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        Vector <String> v = new Vector<String>();
+
+        
+
+    }
+}
---
+++ src/Stack.java
@@ -0,0 +1,19 @@
+/**
+ * Interface para las funciones de Stack de las operaciones del compilador.
+ */
+
+
+public interface Stack<T> {
+
+    void push(T num); //Agregar.
+
+    T pop(); //Eliminar el último valor del vector.
+
+    T peek(); //Toma el valor.
+
+    int size(); //Retorna el tamaño del vector.
+
+    boolean empty(); //Devuelve el estado del vector.
+
+
+}
